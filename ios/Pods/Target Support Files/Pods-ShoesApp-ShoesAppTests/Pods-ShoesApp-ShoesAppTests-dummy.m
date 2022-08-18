1d1b7a787baec3f3e7f528ea0c7e2e8d10b9765f
thaovy <thaovyluuthanh@gmail.com>
2022-08-18 11:46:36 +0700

    update

---
+++ ios/Pods/Target Support Files/Pods-ShoesApp-ShoesAppTests/Pods-ShoesApp-ShoesAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShoesApp_ShoesAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_ShoesApp_ShoesAppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-ShoesApp/Pods-ShoesApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShoesApp : NSObject
+@end
+@implementation PodsDummy_Pods_ShoesApp
+@end
